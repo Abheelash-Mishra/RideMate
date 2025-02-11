27c55bf6475267337d5c0b3a0c32e7dbf59fa8c6
Abheelash Mishra <abheelash.mishra@batonsystems.com>
2025-02-11 15:15:41 +0530

    Created driver class

---
+++ src/models/Driver.java
@@ -0,0 +1,19 @@
+package models;
+
+public class Driver {
+    public final String driverID;
+    public final int[] coordinates;
+    public boolean available = true;
+
+    public Driver(String driverID, int x_coordinate, int y_coordinate) {
+        this.driverID = driverID;
+        this.coordinates = new int[]{x_coordinate, y_coordinate};
+
+        System.out.println("Created driver - " + driverID);
+    }
+
+
+    public void updateAvailability() {
+        available = !available;
+    }
+}
