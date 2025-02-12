4d5dfdf779a8e8cd2499e9e7395fef68fa52064c
Abheelash Mishra <abheelash.mishra@batonsystems.com>
2025-02-12 11:27:09 +0530

    Implemented function to match riders

--- src/services/RideService.java
+++ src/services/RideService.java
@@ -17,17 +17,67 @@ public class RideService {
         System.err.println("Added new rider - " + riderID);
     }
 
-    public matchRider(String riderID) {
+    public void matchRider(String riderID, DriverService driverService) {
+        final double LIMIT = 5.0;
+        int[] riderCoordinates = riderDetails.get(riderID).coordinates;
 
+        HashMap<String, Driver> allDrivers = driverService.driverDetails;
+        PriorityQueue<DriverDistancePair> nearestDrivers = new PriorityQueue<>(new Comparator<DriverDistancePair>() {
+            @Override
+            public int compare(DriverDistancePair pair1, DriverDistancePair pair2) {
+                if (pair1.distance < pair2.distance) {
+                    return -1;
+                }
+                else if (pair1.distance > pair2.distance) {
+                    return 1;
+                }
+
+                return pair1.ID.compareTo(pair2.ID);
+            }
+        });
+
+        for(String driverID : allDrivers.keySet()) {
+            Driver driver = allDrivers.get(driverID);
+            if(driver.available) {
+                double distance = DistanceUtility.calculate(riderCoordinates, driver.coordinates);
+                System.err.println(distance);
+
+                if (distance <= LIMIT) {
+                    DriverDistancePair pair = new DriverDistancePair(driverID, distance);
+                    nearestDrivers.add(pair);
+                }
+            }
+        }
+
+        driversMatched(nearestDrivers);
     }
-}
 
-class DriverDistancePair {
-    public int distance;
-    public String ID;
+    public void driversMatched(PriorityQueue<DriverDistancePair> nearestDrivers) {
+        if (nearestDrivers.isEmpty()) {
+            System.out.println("NO_DRIVERS_AVAILABLE");
+            return;
+        }
 
-    public DriverDistancePair(int distance, String ID) {
-        this.distance = distance;
-        this.ID = ID;
+        int size = Math.min(nearestDrivers.size(), 5);
+        System.out.print("DRIVERS_MATCHED ");
+
+        for (int i = 0; i < size; i++) {
+            DriverDistancePair driver = nearestDrivers.poll();
+            if (driver != null) {
+                System.out.print(driver.ID + " ");
+            }
+        }
+        System.out.println();
+    }
+
+
+    public static class DriverDistancePair {
+        public String ID;
+        public double distance;
+
+        public DriverDistancePair(String ID, double distance) {
+            this.ID = ID;
+            this.distance = distance;
+        }
     }
 }
