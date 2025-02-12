98b25df35b84835613fce2e739fa869c5cf5bda1
Abheelash Mishra <abheelash.mishra@batonsystems.com>
2025-02-12 14:20:45 +0530

    Created class to store ride details

---
+++ src/models/Ride.java
@@ -0,0 +1,21 @@
+package models;
+
+public class Ride {
+    public String riderID;
+    public String driverID;
+    public int[] destinationCoordinates;
+    public int timeTakenInMins;
+    public boolean hasFinished;
+
+    public Ride(String riderID, String driverID) {
+        this.riderID = riderID;
+        this.driverID = driverID;
+        this.hasFinished = false;
+    }
+
+    public void finishRide(int dest_x_coordinate, int dest_y_coordinate, int timeTakenInMins) {
+        this.destinationCoordinates = new int[]{dest_x_coordinate, dest_y_coordinate};
+        this.timeTakenInMins = timeTakenInMins;
+        this.hasFinished = true;
+    }
+}
