3cef06e498ac6547355d421422a0d3c98ec98402
Abheelash Mishra <abheelash.mishra@batonsystems.com>
2025-02-13 11:12:21 +0530

    Implemented multiple testcases to handle various scenarios

--- tests/RiderAppTests.java
+++ tests/RiderAppTests.java
@@ -8,7 +8,6 @@ import java.io.*;
 class RiderAppTests {
     private final InputStream originalSystemIn = System.in;
     private final PrintStream originalSystemOut = System.out;
-    private ByteArrayInputStream testInput;
     private ByteArrayOutputStream testOutput;
 
     @BeforeEach
@@ -24,7 +23,7 @@ class RiderAppTests {
     }
 
     @Test
-    void Testcase1() {
+    void ShortTestcase() {
         String input = """
                 ADD_DRIVER D1 1 1
                 ADD_DRIVER D2 4 5
@@ -46,9 +45,8 @@ class RiderAppTests {
         runTest(input, expectedOutput);
     }
 
-
     @Test
-    void Testcase2() {
+    void LongerTestcase() {
         String input = """
                 ADD_DRIVER D1 0 1
                 ADD_DRIVER D2 2 3
@@ -79,9 +77,161 @@ class RiderAppTests {
         runTest(input, expectedOutput);
     }
 
+    @Test
+    void NoDriversAvailableAtAll() {
+        String input = """
+                ADD_RIDER R1 3 5
+                ADD_RIDER R2 1 1
+                MATCH R1
+                MATCH R2
+                """;
+
+        String expectedOutput = """
+                NO_DRIVERS_AVAILABLE
+                NO_DRIVERS_AVAILABLE
+                """;
+
+        runTest(input, expectedOutput);
+    }
+
+    @Test
+    void StartExistingRide_ThrowsException() {
+        String input = """
+                ADD_DRIVER D1 0 1
+                ADD_DRIVER D2 2 3
+                ADD_RIDER R1 3 5
+                ADD_DRIVER D3 4 2
+                ADD_RIDER R2 1 1
+                MATCH R1
+                MATCH R2
+                START_RIDE RIDE-101 1 R1
+                START_RIDE RIDE-101 1 R2
+                STOP_RIDE RIDE-101 10 2 48
+                BILL RIDE-101
+                """;
+
+        String expectedOutput = """
+                DRIVERS_MATCHED D2 D3 D1
+                DRIVERS_MATCHED D1 D2 D3
+                RIDE_STARTED RIDE-101
+                INVALID_RIDE
+                RIDE_STOPPED RIDE-101
+                BILL RIDE-101 D2 234.6
+                """;
+
+        runTest(input, expectedOutput);
+    }
+
+    @Test
+    void StartRideWithBusyDriver_ThrowsException() {
+        String input = """
+                ADD_DRIVER D1 0 1
+                ADD_DRIVER D2 2 3
+                ADD_RIDER R1 3 5
+                ADD_DRIVER D3 4 2
+                ADD_RIDER R2 1 1
+                MATCH R1
+                MATCH R2
+                START_RIDE RIDE-101 1 R1
+                START_RIDE RIDE-102 2 R2
+                STOP_RIDE RIDE-101 10 2 48
+                BILL RIDE-101
+                """;
+
+        String expectedOutput = """
+                DRIVERS_MATCHED D2 D3 D1
+                DRIVERS_MATCHED D1 D2 D3
+                RIDE_STARTED RIDE-101
+                INVALID_RIDE
+                RIDE_STOPPED RIDE-101
+                BILL RIDE-101 D2 234.6
+                """;
+
+        runTest(input, expectedOutput);
+    }
+
+    @Test
+    void StartRideWithNonExistentDriver_ThrowsException() {
+        String input = """
+                ADD_DRIVER D1 0 1
+                ADD_DRIVER D2 2 3
+                ADD_RIDER R1 3 5
+                ADD_DRIVER D3 4 2
+                ADD_RIDER R2 1 1
+                MATCH R1
+                MATCH R2
+                START_RIDE RIDE-101 1 R1
+                START_RIDE RIDE-102 4 R2
+                STOP_RIDE RIDE-101 10 2 48
+                BILL RIDE-101
+                """;
+
+        String expectedOutput = """
+                DRIVERS_MATCHED D2 D3 D1
+                DRIVERS_MATCHED D1 D2 D3
+                RIDE_STARTED RIDE-101
+                INVALID_RIDE
+                RIDE_STOPPED RIDE-101
+                BILL RIDE-101 D2 234.6
+                """;
+
+        runTest(input, expectedOutput);
+    }
+
+    @Test
+    void StopAlreadyCompletedRide_ThrowsException() {
+        String input = """
+                ADD_DRIVER D1 1 1
+                ADD_DRIVER D2 4 5
+                ADD_DRIVER D3 2 2
+                ADD_RIDER R1 0 0
+                MATCH R1
+                START_RIDE RIDE-001 2 R1
+                STOP_RIDE RIDE-001 4 5 32
+                STOP_RIDE RIDE-001 1 1 10
+                BILL RIDE-001
+                """;
+
+        String expectedOutput = """
+                DRIVERS_MATCHED D1 D3
+                RIDE_STARTED RIDE-001
+                RIDE_STOPPED RIDE-001
+                INVALID_RIDE
+                BILL RIDE-001 D3 186.7
+                """;
+
+        runTest(input, expectedOutput);
+    }
+
+    @Test
+    void StopNonExistentRide_ThrowsException() {
+        String input = """
+                ADD_DRIVER D1 1 1
+                ADD_DRIVER D2 4 5
+                ADD_DRIVER D3 2 2
+                ADD_RIDER R1 0 0
+                MATCH R1
+                START_RIDE RIDE-001 2 R1
+                STOP_RIDE RIDE-001 4 5 32
+                STOP_RIDE RIDE-999 1 1 10
+                BILL RIDE-001
+                """;
+
+        String expectedOutput = """
+                DRIVERS_MATCHED D1 D3
+                RIDE_STARTED RIDE-001
+                RIDE_STOPPED RIDE-001
+                INVALID_RIDE
+                BILL RIDE-001 D3 186.7
+                """;
+
+        runTest(input, expectedOutput);
+    }
+
+
 
     private void runTest(String input, String expectedOutput) {
-        testInput = new ByteArrayInputStream(input.getBytes());
+        ByteArrayInputStream testInput = new ByteArrayInputStream(input.getBytes());
         System.setIn(testInput);
 
         RiderApp.reset();
