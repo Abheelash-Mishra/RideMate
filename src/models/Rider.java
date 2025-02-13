9572c261772544b5b5e1a063e01ba1ae3ac6410d
Abheelash Mishra <abheelash.mishra@batonsystems.com>
2025-02-13 15:22:09 +0530

    Added walletAmount and its relevant functions to add or deduct

--- src/models/Rider.java
+++ src/models/Rider.java
@@ -3,9 +3,23 @@ package models;
 public class Rider {
     public String riderID;
     public int[] coordinates;
+    private float walletAmount = 0;
 
     public Rider(String riderID, int x_coordinate, int y_coordinate) {
         this.riderID = riderID;
         this.coordinates = new int[]{x_coordinate, y_coordinate};
+    }
+
+    public void addMoney(float amount) {
+        walletAmount += amount;
+    }
+
+    public void deductMoney(float amount) {
+        if (walletAmount <= amount) {
+            System.out.println("LOW_BALANCE");
+            return;
+        }
+
+        walletAmount -= amount;
     }
 }
