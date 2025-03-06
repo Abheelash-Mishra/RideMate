8bba7e8d10be5bd81db6e75bdec42221fa2b1654
Abheelash Mishra <abheelash.mishra@batonsystems.com>
2025-03-06 12:09:48 +0530

    Add ride status enum

--- src/main/java/org/example/models/Ride.java
+++ src/main/java/org/example/models/Ride.java
@@ -7,19 +7,19 @@ public class Ride {
     private String riderID;
     private String driverID;
     private int[] destinationCoordinates;
+    private RideStatus status;
     private int timeTakenInMins;
-    private boolean finished;
     private float bill;
 
     public Ride(String riderID, String driverID) {
         this.riderID = riderID;
         this.driverID = driverID;
-        this.finished = false;
+        this.status = RideStatus.STARTED;
     }
 
     public void finishRide(int dest_x_coordinate, int dest_y_coordinate, int timeTakenInMins) {
         this.destinationCoordinates = new int[]{dest_x_coordinate, dest_y_coordinate};
         this.timeTakenInMins = timeTakenInMins;
-        this.finished = true;
+        this.status = RideStatus.FINISHED;
     }
 }
