435d88fc333511b7558e3df6cf6a0ad77807a458
Abheelash Mishra <abheelash.mishra@batonsystems.com>
2025-03-11 11:55:01 +0530

    Remove manual instance creation of payment methods

--- src/main/java/org/example/models/PaymentMethodType.java
+++ src/main/java/org/example/models/PaymentMethodType.java
@@ -1,22 +1,6 @@
 package org.example.models;
 
-import org.example.services.impl.CardPayment;
-import org.example.services.impl.CashPayment;
-import org.example.services.impl.UpiPayment;
-import org.example.services.impl.WalletPayment;
-import org.example.services.IPayment;
-
-
 public enum PaymentMethodType {
     CASH, CARD, UPI, WALLET;
-
-    public IPayment getPaymentMethod() {
-        return switch (this) {
-            case CASH -> new CashPayment();
-            case CARD -> new CardPayment();
-            case UPI -> new UpiPayment();
-            case WALLET -> new WalletPayment();
-        };
-    }
 }
 
