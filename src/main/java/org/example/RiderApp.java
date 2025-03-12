8740d5ccc88f5368177269456ea075940c3fd035
Abheelash Mishra <abheelash.mishra@batonsystems.com>
2025-03-12 14:40:19 +0530

    Update main class to allow CLI implementation

--- src/main/java/org/example/RiderApp.java
+++ src/main/java/org/example/RiderApp.java
@@ -2,10 +2,18 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class RiderApp {
     public static void main(String[] args) {
-        SpringApplication.run(RiderApp.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(RiderApp.class, args);
+
+        if (args.length > 0 && "cli".equalsIgnoreCase(args[0])) {
+            RiderAppCLI riderAppCLI = context.getBean(RiderAppCLI.class);
+            riderAppCLI.start();
+            context.close();
+        }
     }
 }
+
