6041ff8c1fbb3074e185992cfaa0c2be93ca419e
Abheelash Mishra <abheelash.mishra@batonsystems.com>
2025-03-18 10:24:44 +0530

    Use Slf4j logging for outputs instead

--- src/main/java/org/example/RiderAppCLI.java
+++ src/main/java/org/example/RiderAppCLI.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import lombok.extern.slf4j.Slf4j;
 import org.example.dto.DriverDTO;
 import org.example.dto.PaymentDetailsDTO;
 import org.example.exceptions.*;
@@ -11,13 +12,13 @@ import org.example.services.AdminService;
 import org.example.services.DriverService;
 import org.example.services.PaymentService;
 import org.example.services.RideService;
-import org.example.services.impl.WalletPayment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Scanner;
 
+@Slf4j
 @Component
 public class RiderAppCLI {
 
@@ -59,12 +60,14 @@ public class RiderAppCLI {
         String[] parts = command.split(" ");
 
         int x_coordinate, y_coordinate, N;
-        String riderID, rideID, driverID;
+        long riderID, rideID, driverID;
+
+        StringBuilder output;
 
         try {
             switch (parts[0]) {
                 case "ADD_DRIVER":
-                    driverID = parts[1];
+                    driverID = Long.parseLong(parts[1]);
                     x_coordinate = Integer.parseInt(parts[2]);
                     y_coordinate = Integer.parseInt(parts[3]);
 
@@ -72,7 +75,7 @@ public class RiderAppCLI {
                     break;
 
                 case "ADD_RIDER":
-                    riderID = parts[1];
+                    riderID = Long.parseLong(parts[1]);
                     x_coordinate = Integer.parseInt(parts[2]);
                     y_coordinate = Integer.parseInt(parts[3]);
 
@@ -80,89 +83,87 @@ public class RiderAppCLI {
                     break;
 
                 case "MATCH":
-                    riderID = parts[1];
+                    riderID = Long.parseLong(parts[1]);
 
-                    List<String> matchedDrivers = rideService.matchRider(riderID).getMatchedDrivers();
+                    List<Long> matchedDrivers = rideService.matchRider(riderID).getMatchedDrivers();
                     if (matchedDrivers.isEmpty()) {
                         throw new NoDriversException();
                     }
 
-                    System.out.print("DRIVERS_MATCHED");
-                    for (String matchedDriver : matchedDrivers) {
-                        System.out.print(" " + matchedDriver);
+                    output = new StringBuilder("DRIVERS_MATCHED");
+                    for (long matchedDriver : matchedDrivers) {
+                        output.append(" ").append(matchedDriver);
                     }
-                    System.out.println();
 
+                    log.info(String.valueOf(output));
                     break;
 
                 case "START_RIDE":
-                    rideID = parts[1];
+                    rideID = Long.parseLong(parts[1]);
                     N = Integer.parseInt(parts[2]);
-                    riderID = parts[3];
+                    riderID = Long.parseLong(parts[3]);
 
                     rideService.startRide(rideID, N, riderID);
-                    System.out.println("RIDE_STARTED " + rideID);
+                    log.info("RIDE_STARTED {}", rideID);
                     break;
 
                 case "STOP_RIDE":
-                    rideID = parts[1];
+                    rideID = Long.parseLong(parts[1]);
                     int dest_x_coordinate = Integer.parseInt(parts[2]);
                     int dest_y_coordinate = Integer.parseInt(parts[3]);
                     int timeTakenInMins = Integer.parseInt(parts[4]);
 
                     rideService.stopRide(rideID, dest_x_coordinate, dest_y_coordinate, timeTakenInMins);
-                    System.out.println("RIDE_STOPPED " + rideID);
+                    log.info("RIDE_STOPPED {}", rideID);
                     break;
 
                 case "RATE_DRIVER":
-                    driverID = parts[1];
+                    driverID = Long.parseLong(parts[1]);
                     float rating = Float.parseFloat(parts[2]);
 
                     float newRating = driverService.rateDriver(driverID, rating).getRating();
-                    System.out.println("CURRENT_RATING " + driverID + " " + newRating);
+                    log.info("CURRENT_RATING {} {}", driverID, newRating);
                     break;
 
                 case "BILL":
-                    rideID = parts[1];
+                    rideID = Long.parseLong(parts[1]);
 
-                    double bill = rideService.billRide(rideID);
+                    rideService.billRide(rideID);
                     Ride currentRide = rideRepository.findById(rideID)
                             .orElseThrow(InvalidRideException::new);
 
-                    System.out.printf("BILL %s %s %.1f\n", rideID, currentRide.getDriver().getDriverID(), bill);
+                    log.info("BILL {} {} {}", rideID, currentRide.getDriver().getDriverID(), currentRide.getBill());
                     break;
 
                 case "PAY":
-                    rideID = parts[1];
+                    rideID = Long.parseLong(parts[1]);
                     String type = parts[2];
 
                     PaymentMethodType paymentMethodType = PaymentMethodType.valueOf(type.toUpperCase());
-                    paymentService.setPaymentMethod(paymentMethodType);
 
-                    PaymentDetailsDTO paymentDetails = paymentService.processPayment(rideID);
+                    PaymentDetailsDTO paymentDetails = paymentService.processPayment(rideID, paymentMethodType);
                     if (paymentDetails.getPaymentStatus() == PaymentStatus.FAILED) {
-                        System.out.println("LOW_BALANCE");
+                        log.info("LOW_BALANCE");
                     }
                     else {
-                        System.out.printf("PAID %s %.1f VIA %s\n", paymentDetails.getReceiverID(), paymentDetails.getAmount(), paymentMethodType);
+                        log.info("PAID {} {} VIA {}", paymentDetails.getReceiverID(), paymentDetails.getAmount(), paymentMethodType);
                     }
                     break;
 
                 case "ADD_MONEY":
-                    riderID = parts[1];
+                    riderID = Long.parseLong(parts[1]);
                     float amount = Float.parseFloat(parts[2]);
 
-                    paymentService.setPaymentMethod(PaymentMethodType.WALLET);
-                    WalletPayment wallet = (WalletPayment) paymentService.getPaymentMethod();
-
-                    float balance = wallet.addMoney(riderID, amount);
-                    System.out.println("CURRENT_BALANCE " + riderID + " " + balance);
+                    float balance = paymentService.addMoney(riderID, amount);
+                    log.info("CURRENT_BALANCE {} {}", riderID, balance);
                     break;
 
                 case "ADMIN_REMOVE_DRIVER":
-                    driverID = parts[1];
+                    driverID = Long.parseLong(parts[1]);
 
-                    if (adminService.removeDriver(driverID)) System.out.println("REMOVED_DRIVER " + driverID);
+                    if (adminService.removeDriver(driverID)) {
+                        log.info("REMOVED_DRIVER {}", driverID);
+                    }
                     break;
 
                 case "ADMIN_LIST_DRIVERS":
@@ -170,16 +171,15 @@ public class RiderAppCLI {
 
                     List<DriverDTO> driverDetails = adminService.listNDriverDetails(N);
                     for (DriverDTO driver: driverDetails) {
-                        System.out.printf("DRIVER_%s (X=%d, Y=%d) RATING %.1f", driver.getDriverID(), driver.getX(), driver.getY(), driver.getRating());
-                        System.out.println();
+                        log.info("DRIVER_{} (X={}, Y={}) RATING {}", driver.getDriverID(), driver.getX(), driver.getY(), driver.getRating());
                     }
                     break;
 
                 case "ADMIN_VIEW_DRIVER_EARNINGS":
-                    driverID = parts[1];
+                    driverID = Long.parseLong(parts[1]);
 
                     float earnings = adminService.getDriverEarnings(driverID).getEarnings();
-                    System.out.printf("DRIVER_EARNINGS %s %.1f\n", driverID, earnings);
+                    log.info("DRIVER_EARNINGS {} {}", driverID, earnings);
                     break;
 
                 default:
