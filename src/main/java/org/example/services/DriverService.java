9fe0488dd92e9fe8d1817cb8be3ba8c56e310f11
Abheelash Mishra <abheelash.mishra@batonsystems.com>
2025-04-17 17:49:12 +0530

    Receive comments about the driver and store it

--- src/main/java/org/example/services/DriverService.java
+++ src/main/java/org/example/services/DriverService.java
@@ -4,5 +4,5 @@ import org.example.dto.DriverRatingDTO;
 
 public interface DriverService {
     long addDriver(String email, String phoneNumber, int x_coordinate, int y_coordinate);
-    DriverRatingDTO rateDriver(long driverID, float rating);
+    DriverRatingDTO rateDriver(long rideID, long driverID, float rating, String comment);
 }
