91ba3d5fa4ebf23c23b4149471d437576f014d84
Abheelash Mishra <abheelash.mishra@batonsystems.com>
2025-04-17 16:24:21 +0530

    Return DTO containing information of all past rides taken

--- src/main/java/org/example/repository/RideRepository.java
+++ src/main/java/org/example/repository/RideRepository.java
@@ -2,8 +2,15 @@ package org.example.repository;
 
 import org.example.models.Ride;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface RideRepository extends JpaRepository<Ride, Long> {
+
+    @Query("SELECT r.rideID, r.driver.driverID, r.destination, r.bill, r.timeTakenInMins FROM Ride r WHERE r.rider.riderID = :riderId ORDER BY r.rideID DESC")
+    List<Object[]> findAllRides(@Param("riderId") long riderId);
 }
